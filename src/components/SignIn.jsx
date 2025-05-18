8224a02c3ee63efdeda15be1d13cae3a63a1c453
unknown <zaman-pro>
2025-05-19 00:44:26 +0600

    sign in fn and update lastSignInTime added

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 
 const SignIn = () => {
+  const { signInUser } = use(AuthContext);
   const handleSignIn = (e) => {
     e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    console.log(email, password);
+
+    signInUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+        const signInInfo = {
+          email,
+          lastSignInTime: result.user?.metadata?.lastSignInTime,
+        };
+        // update last sign in to the database
+        fetch("http://localhost:3000/users", {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(signInInfo),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="card bg-base-100 max-w-sm mx-auto shrink-0 shadow-2xl">
@@ -26,7 +58,7 @@ const SignIn = () => {
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
-          <button className="btn btn-neutral mt-4">Sign Up</button>
+          <button className="btn btn-neutral mt-4">Sign In</button>
         </form>
       </div>
     </div>
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { AuthContext } from "./AuthContext";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import { auth } from "../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
@@ -8,7 +11,11 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const userInfo = { createUser };
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const userInfo = { createUser, signInUser };
 
   return <AuthContext value={userInfo}>{children}</AuthContext>;
 };
