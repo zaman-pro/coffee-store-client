3db3c5310ae58efc7c5becc9585d388d6465e4c9
unknown <zaman-pro>
2025-05-18 21:53:39 +0600

    user delete by id

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const SignIn = () => {
-  return <div>This is sign in</div>;
+  const handleSignIn = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="card bg-base-100 max-w-sm mx-auto shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-5xl font-bold">Sign In now!</h1>
+        <form onSubmit={handleSignIn} className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            name="email"
+            className="input"
+            placeholder="Email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="input"
+            placeholder="Password"
+          />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,14 +1,48 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const initialUsers = useLoaderData();
 
   const [users, setUsers] = useState(initialUsers);
 
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+
+              const remainingUsers = users.filter((user) => user._id !== id);
+              setUsers(remainingUsers);
+            }
+
+            console.log(data);
+          });
+      }
+    });
+  };
+
   return (
     <div>
-      <h1>Users : {initialUsers.length}</h1>
+      <h1>Users : {users.length}</h1>
 
       <div className="overflow-x-auto">
         <table className="table">
@@ -17,12 +51,13 @@ const Users = () => {
             <tr>
               <th>
                 <label>
-                  <input type="checkbox" className="checkbox" />
+                  {/* <input type="checkbox" className="checkbox" /> */}
+                  SN
                 </label>
               </th>
               <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Phone</th>
+              <th>Email</th>
               <th></th>
             </tr>
           </thead>
@@ -41,27 +76,28 @@ const Users = () => {
                     <div className="avatar">
                       <div className="mask mask-squircle h-12 w-12">
                         <img
-                          src="https://img.daisyui.com/images/profile/demo/2@94.webp"
+                          src={user.photoURL}
                           alt="Avatar Tailwind CSS Component"
                         />
                       </div>
                     </div>
                     <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
+                      <div className="font-bold">{user.name}</div>
+                      <div className="text-sm opacity-50">{user.address}</div>
                     </div>
                   </div>
                 </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
-                </td>
-                <td>Purple</td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
                 <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
+                  <button className="btn btn-xs">V</button>
+                  <button className="btn btn-xs">E</button>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-xs"
+                  >
+                    X
+                  </button>
                 </th>
               </tr>
             ))}
