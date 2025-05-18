7df69a2bf27320eb74d1b5450d254d6b354c1384
unknown <zaman-pro>
2025-05-18 18:30:34 +0600

    profile info added in db

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import Swal from "sweetalert2";
 
 const SignUp = () => {
   const { createUser } = use(AuthContext);
@@ -11,16 +12,41 @@ const SignUp = () => {
 
     const form = e.target;
     const formData = new FormData(form);
-    const email = formData.get("email");
-    const password = formData.get("password");
 
-    console.log(email, password);
+    const { email, password, ...userProfile } = Object.fromEntries(
+      formData.entries()
+    );
+    // const email = formData.get("email");
+    // const password = formData.get("password");
+
+    console.log(email, password, userProfile);
 
     // create user in the firebase
 
     createUser(email, password)
       .then((result) => {
         console.log(result);
+
+        // save profile info in the db
+        fetch("http://localhost:3000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(userProfile),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your account is created",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          });
       })
       .catch((error) => {
         // const errorCode = error.code;
@@ -35,6 +61,29 @@ const SignUp = () => {
       <div className="card-body">
         <h1 className="text-5xl font-bold">Sign Up now!</h1>
         <form onSubmit={handleSignUp} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" name="name" className="input" placeholder="Name" />
+          <label className="label">Address</label>
+          <input
+            type="text"
+            name="address"
+            className="input"
+            placeholder="Address"
+          />
+          <label className="label">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            className="input"
+            placeholder="Phone"
+          />
+          <label className="label">PhotoURL</label>
+          <input
+            type="text"
+            name="photoURL"
+            className="input"
+            placeholder="PhotoURL"
+          />
           <label className="label">Email</label>
           <input
             type="email"
